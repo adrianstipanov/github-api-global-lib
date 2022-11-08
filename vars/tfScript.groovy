9facc48a3c5e814adfe5998ffdd09cc998e3c241
Adrian Stipanov <46926843+adrianstipanov@users.noreply.github.com>
2022-11-08 18:38:22 +0100

    Update tfScript.groovy

--- vars/tfScript.groovy
+++ vars/tfScript.groovy
@@ -1,6 +1,7 @@
 def call(Object terraformObject, Closure scriptClosure) {
   return {
     echo "$this"
+    sh 'printenv | sort'
     terraformObject.context = this
     scriptClosure.delegate = terraformObject
     scriptClosure.resolveStrategy = Closure.DELEGATE_FIRST
